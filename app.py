542a585005cec66f488dc42a65dff5485697e82d
mtalal517 <mtalalqureshi5@gmail.com>
2025-02-19 15:51:58 +0500

    deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deploying Flask App at Vercel"
+    return "Deploying Flask App at Vercel  for MLOPS TASK 01"
 
 if __name__ == "__main__":
     app.run(debug=True)
